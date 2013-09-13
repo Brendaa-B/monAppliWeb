c7223f745f1e9f78c5e037a0198ba6214eafb1a5
EMDS <emds@cned.fr>
2013-09-13 11:50:33 +0200

    Test conflits 1

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 	<div id="contenuPage">
             <?php if (!isset($_GET['page']))
                     include 'calculatrice.html';
-                  if (isset($_GET['page']))
-                      if ($_GET['page']=="nouscontacter")
-                          include 'nouscontacter.html';
                       if ($_GET['page']=="articles")
                           include 'articles.html';?>
         </div>
