f7c09844170e61d44189539516072bb176271a46
Brenda BARBE <brenda.barbe.83@gmail.com>
2013-09-13 11:32:52 +0200

    Insertion du formulaire de contact
    Signed-off-by: Brenda BARBE <brenda.barbe.83@gmail.com>

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php include 'header.html'; ?>
-	<div id="contenuPage"><?php include 'calculatrice.html'; ?></div>
+	<div id="contenuPage">
+            <?php if (!isset($_GET['page']))
+                    include 'calculatrice.html';
+                  if (isset($_GET['page']))
+                      if ($_GET['page']=="nouscontacter")
+                          include 'nouscontacter.html';?>
+        </div>
 <?php include 'footer.html'; ?>
